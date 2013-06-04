061b9e2cc5df0566adcb8bb33448c404ebc5f55a
acacheung <acacheung@gmail.com>
2013-06-04 19:50:17 -0400

    about to delete spec controllers dir

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,3 +1,5 @@
 class Project < ActiveRecord::Base
   attr_accessible :codebase_url, :contact_email, :description, :name, :url
+
+  validates_presence_of :codebase_url, :contact_email, :description, :name, :url
 end
