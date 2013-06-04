6e081568c7844e9f69ee5abe0f7c26e3d67773fa
acacheung <acacheung@gmail.com>
2013-06-04 19:55:52 -0400

    passed submit project unit and accesptance tests

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,5 +1,9 @@
 class Project < ActiveRecord::Base
   attr_accessible :codebase_url, :contact_email, :description, :name, :url
 
-  validates_presence_of :codebase_url, :contact_email, :description, :name, :url
+  validates_presence_of :codebase_url, :description, :name, :url
+
+  validates :url, :format => { :with => /[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?$/ }
+  validates :codebase_url, :format => { :with => /[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?$/ }
+  validates :contact_email, :format => { :with=>/\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i }, :allow_blank => true
 end
